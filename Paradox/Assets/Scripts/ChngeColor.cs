298ec6d8c7b34151cfa234a8fe8ac7d25359a194
Edwin Gamboa <edwjov92@gmail.com>
2015-12-11 22:57:19 +0100

    -Player walks and stop using Cardboard trigger. -Player interacts with a sphere looking at it. -Player interacts with a sphere looking at it for 2 seconds.

---
+++ Paradox/Assets/Scripts/ChngeColor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChngeColor : MonoBehaviour {
+
+    void Update()
+    {
+        if (Cardboard.SDK.Triggered)
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+    }
+}
---
+++ Paradox/Assets/Scripts/ObjectController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectController : MonoBehaviour {
+
+    private CardboardHead head;
+    private float delay = 0.0f;
+
+    void Start()
+    {
+        head = Camera.main.GetComponent<StereoController>().Head;
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+        bool isLookedAt = GetComponent<Collider>().Raycast(head.Gaze, out hit, Mathf.Infinity);
+        // if looking at object for 2 seconds, enable/disable autowalk
+        if (isLookedAt && Time.time > delay)
+        {
+            Debug.Log("ahora cambio de color");
+            GetComponent<Renderer>().material.color = Color.blue;
+            
+        }
+        // currently looking at object
+        else if (isLookedAt)
+        {
+            Debug.Log("estas mirandome");
+            GetComponent<Renderer>().material.color = Color.yellow;
+            
+        }
+        // not looking at object
+        else if (!isLookedAt)
+        {
+            Debug.Log("ahora no me miras");
+            GetComponent<Renderer>().material.color = Color.red;
+            delay = Time.time + 2.0f;
+        }
+    }
+}
