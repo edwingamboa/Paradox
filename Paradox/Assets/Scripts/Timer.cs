f37d99ce4ebdf52d29b4d72482165f5ab2785a33
Sebastian Lasa <lasa.sebas@gmail.com>
2016-01-08 02:00:21 +0200

    Texturas paredes y piso. Escena Game Over. Gravedad del jugador.

--- Paradox/Assets/Scripts/Timer.cs
+++ Paradox/Assets/Scripts/Timer.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour {
     public int minutes = 2;
@@ -19,11 +20,21 @@ public class Timer : MonoBehaviour {
             minutes -= 1;
         }
 
-        if (minutes > 0) {
-            tm.text = Mathf.RoundToInt(minutes).ToString() + ":" + Mathf.RoundToInt(seconds).ToString();
+        if (minutes >= 0) {
+            if (seconds < 9.0f || seconds.Equals(9.0f))
+            {
+                tm.text = "0" + Mathf.RoundToInt(minutes).ToString() + ":0" + Mathf.RoundToInt(seconds).ToString();
+            }
+            else {
+                tm.text = "0" + Mathf.RoundToInt(minutes).ToString() + ":" + Mathf.RoundToInt(seconds).ToString();
+            }
         }
         else {
-            tm.text = "GAME OVER";
+            this.GameOver();
         }
+    }
+
+    void GameOver() {
+        SceneManager.LoadScene("GameOver");
     }
 }
